e2e6bd2dd4323560bda89649bef78a1c05f14922
Alex Brinkman <abrink@commercekitchen.com>
2018-12-08 22:54:21 -0700

    [CHG] Change theme, make a bit more responsive

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import BoardDisplay from './BoardDisplay.js';
 
 class App extends Component {
 
+  state = {
+    position: [
+      [true, true, true],
+      [true, true, true, true],
+      [true, true, true, true, true]
+    ],
+    gameOver: false,
+    message: ''
+  };
+
   constructor(props) {
     super(props);
-    this.state = {
-      position: [
-        [true, true, true],
-        [true, true, true, true],
-        [true, true, true, true, true]
-      ],
-      gameOver: false,
-      message: ''
-    };
-
     this.togglePosition = this.togglePosition.bind(this);
     this.makeMove = this.makeMove.bind(this);
   }
